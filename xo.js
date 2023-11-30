59b3a7b88786ce05b7fb725caa5a42b6fb9075f6
ZoranNikolov <zoran902@gmail.com>
2023-11-30 15:52:21 +0200

    refactored

--- xo.js
+++ xo.js
@@ -1,15 +1,6 @@
 function XO(str) {
-	let x = 0;
-	let o = 0;
-	str.split('').forEach((el) => {
-		if (el.toLowerCase() === "x") {
-			x++;
-		}
-		if (el.toLowerCase() === "o") {
-			o++;
-		}
-	});
-	return x === o;
+	str = str.toLowerCase().split("");
+	return (
+		str.filter((x) => x === "x").length === str.filter((x) => x === "o").length
+	);
 }
-
-console.log(XO("ooxx"), XO("xooxx"), XO("ooxXm"), XO("zpzpzpp"), XO("zzoo"));
