88e9bd99c14980f2ffc23cfa34662cde14d46751
ZoranNikolov <zoran902@gmail.com>
2023-12-01 16:03:31 +0200

    new tasks

---
+++ solution.js
@@ -0,0 +1,21 @@
+/**
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  "" 
+*/
+
+function solution(string) {
+	string = string.split("").map(function (el) {
+		if (el === el.toUpperCase()) {
+			el = " " + el;
+		}
+		return el;
+	});
+	return string.join("");
+}
+
+solution("camelCasing");
+solution("");
